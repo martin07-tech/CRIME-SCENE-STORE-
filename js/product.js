5d21210110bcfa0e1f6c915c708733cde61ecf17
MartinSyntax <martinmoepya07@gmail.com>
2026-06-12 11:55:29 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -12,30 +12,303 @@ window.addEventListener("load", () => {
 // ======================================
 
 const products = [
-    {
-        id: 1,
-        name: "Case NO:1 Tee",
-        price: 220,
-        category: "tshirt",
-        sizes: ["S", "M", "L", "XL"],
-        image: "assets/images/products/IMG-20260512-WA0014.jpg"
-    },
-    {
-        id: 2,
-        name: "CS Hoodie",
-        price: 350,
-        category: "hoodie",
-        sizes: ["S", "M", "L", "XL"],
-        image: "assets/images/products/IMG-20260512-WA0007.jpg"
-    },
-    {
-        id: 3,
-        name: "Crime Cap",
-        price: 120,
-        category: "cap",
-        sizes: ["One Size"],
-        image: "assets/images/products/1778690174947.png"
-    }
+  {
+    id: 1,
+    name: "Case NO:1 Tee",
+    price: "R220",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0014.jpg"
+},
+
+{
+    id: 2,
+    name: "CS Hoodie",
+    price: "R350",
+    category: "hoodie",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0007.jpg"
+},
+
+{
+    id: 3,
+    name: "Crime Cap",
+    price: "R120",
+    category: "cap",
+    sizes: ["One Size"],
+    image: "assets/images/products/1778690174947.png"
+},
+
+{
+    id: 6,
+    name: "Crime Drip tee",
+    price: "R220",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0011.jpg"
+},
+
+{
+    id: 7,
+    name: "T-crime shirt (Brown)",
+    price: "R220",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0012.jpg"
+},
+
+{
+    id: 8,
+    name: "BIG BOYS tee",
+    price: "R220",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0013.jpg"
+},
+
+{
+    id: 9,
+    name: "Classic Crime tee",
+    price: "R220",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0014.jpg"
+},
+
+{
+    id: 10,
+    name: "Any Means Hoodie (pink)",
+    price: "R385",
+    category: "hoodie",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0040.jpg"
+},
+
+{
+    id: 11,
+    name: "Discovery shirt (grey)",
+    price: "R300",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0039.jpg"
+},
+
+{
+    id: 12,
+    name: "Discovery shirt (black)",
+    price: "R300",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0038.jpg"
+},
+
+{
+    id: 13,
+    name: "Any Means Hoodie (white)",
+    price: "R385",
+    category: "hoodie",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0035.jpg"
+},
+
+{
+    id: 14,
+    name: "Any Means Hoodie (blue)",
+    price: "R385",
+    category: "hoodie",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0034.jpg"
+},
+
+{
+    id: 15,
+    name: "Any Means Hoodie (brown)",
+    price: "R385",
+    category: "hoodie",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0033.jpg"
+},
+
+{
+    id: 16,
+    name: "Crime Drip shirt",
+    price: "R250",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0032.jpg"
+},
+
+{
+    id: 17,
+    name: "Discovery shirt (purpule)",
+    price: "R300",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0036.jpg"
+},
+
+{
+    id: 18,
+    name: "Evidence Drip tee",
+    price: "R230",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0031.jpg"
+},
+
+{
+    id: 19,
+    name: "CRIME SCENE P Hoodie (green)",
+    price: "R375",
+    category: "hoodie",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0030.jpg"
+},
+
+{
+    id: 20,
+    name: "CRC shirt",
+    price: "R230",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0029.jpg"
+},
+
+{
+    id: 21,
+    name: "CRIME SCENE P Hoodie (purpule)",
+    price: "R360",
+    category: "hoodie",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0028.jpg"
+},
+
+{
+    id: 22,
+    name: "Evidence Drip shrt",
+    price: "R220",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0027.jpg"
+},
+
+{
+    id: 23,
+    name: "case Close tee",
+    price: "R250",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0025.jpg"
+},
+
+{
+    id: 24,
+    name: "BIG BOY tee (pitch)",
+    price: "R220",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0024.jpg"
+},
+
+{
+    id: 25,
+    name: "Street Crime shirt (black)",
+    price: "R230",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0022.jpg"
+},
+
+{
+    id: 26,
+    name: "Street Crime shirt (purpule)",
+    price: "R230",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0021.jpg"
+},
+
+{
+    id: 27,
+    name: "Case Secured tee",
+    price: "R250",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0020.jpg"
+},
+
+{
+    id: 28,
+    name: "Crim Drip shirt (white)",
+    price: "R250",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0019.jpg"
+},
+
+{
+    id: 29,
+    name: "Crime Drip shirt (purpule)",
+    price: "R250",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0018.jpg"
+},
+
+{
+    id: 30,
+    name: "Case Closed tee (pink)",
+    price: "R250",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0017.jpg"
+},
+
+{
+    id: 31,
+    name: "Case Close tee (black)",
+    price: "R250",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0016.jpg"
+},
+
+{
+    id: 32,
+    name: "Case 001 shirt (white)",
+    price: "R220",
+    category: "tshirt",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0015.jpg"
+},
+
+{
+    id: 33,
+    name: "polioce line Hoodie (white)",
+    price: "R350",
+    category: "hoodie",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260203-WA0005.jpg"
+},
+
+{
+    id: 34,
+    name: "CRIME SCENE P Hoodie (black)",
+    price: "R375",
+    category: "hoodie",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0009.jpg"
+},
+
+{
+    id: 35,
+    name: "CS Hoodie",
+    price: "R360",
+    category: "hoodie",
+    sizes: ["S", "M", "L", "XL"],
+    image: "assets/images/products/IMG-20260512-WA0044.jpg"
+}
+
 ];
 
 
